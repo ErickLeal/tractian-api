f295cdb6d5ca5557631503e4b568bbaf10d31143
Erick <erick.azapfy@gmail.com>
2023-06-20 22:02:54 -0300

    Atlas config

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import routes from "./routes/routes"
 const app = express();
 const port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://test:test-password@mongodb:27017/?authMechanism=DEFAULT');
+mongoose.connect('mongodb+srv://erick:tractianerick@cluster0.ykzetjp.mongodb.net/?retryWrites=true&w=majority');
 
 app.use(express.json()); 
 app.use(routes)
