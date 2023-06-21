7cc241965c62eb88490b9d8f6eab2f7ca9075bd7
Erick <erick.azapfy@gmail.com>
2023-06-20 21:43:28 -0300

    Config heroku

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,13 @@ import mongoose from 'mongoose';
 import routes from "./routes/routes"
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 mongoose.connect('mongodb://test:test-password@mongodb:27017/?authMechanism=DEFAULT');
 
 app.use(express.json()); 
 app.use(routes)
 
-app.listen(3000 , () => {
+app.listen(port , () => {
     console.log("Server listening");
 });
